b880c69417ccf93ab37d90405a794912f4aead09
Arthur Carlos Jacobina Pires Filho <129566256+Arthur-Jacobina@users.noreply.github.com>
2024-03-23 01:52:05 -0300

    add dynamic images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,21 @@ import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameReducer,
   FrameInput,
+  FrameReducer,
   NextServerPageProps,
+  getFrameMessage,
   getPreviousFrame,
   useFramesReducer,
 } from "frames.js/next/server";
 import Link from "next/link";
 import { currentURL } from "./utils";
-import { createDebugUrl } from "./debug";
+import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from "./debug";
 
 type State = {
   pageIndex: number;
+  active: string;
+  total_button_presses: number;
 };
 
 const imgs: {
@@ -32,8 +35,7 @@ const imgs: {
   },
 ];
 
-const initialState: State = { pageIndex: 0 };
-
+const initialState: State = { pageIndex: 0 , active: "1", total_button_presses: 0 };
 
 //Edit here for changes in button behaviour
 const reducer: FrameReducer<State> = (state, action) => {
@@ -43,42 +45,94 @@ const reducer: FrameReducer<State> = (state, action) => {
     pageIndex: buttonIndex
     ? (state.pageIndex + (buttonIndex === 2 ? 1 : -1)) % imgs.length
     : state.pageIndex,
+    total_button_presses: state.total_button_presses + 1,
+    active: action.postBody?.untrustedData.buttonIndex
+      ? String(action.postBody?.untrustedData.buttonIndex)
+      : "1",
   };
 };  
+
 
 
 // This is a react server component only
 export default async function Home({ searchParams }: NextServerPageProps) {
   const url = currentURL("/");
   const previousFrame = getPreviousFrame<State>(searchParams);
-  const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
+  const frameMessage = await getFrameMessage(previousFrame.postBody, {
+    hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+  });
 
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
 
+  const [state] = useFramesReducer<State>(
+    reducer,
+    initialState,
+    previousFrame
+  );
 
+  console.log("info: state is:", state);
   // then, when done, return next frame
   return (
-    <div>
-      <FrameContainer
-        pathname="/"
-        postUrl="/frames"
-        state={state}
-        previousFrame={previousFrame}
-      >
-        <FrameImage
-          src={imgs[state.pageIndex]!.src}
-          aspectRatio="1:1"
-        ></FrameImage>
-        <FrameInput
-          text="sign our newsletter"
+    <div className="p-4">
+    frames.js starter kit. The Template Frame is on this page, it&apos;s in
+    the html meta tags (inspect source).{" "}
+    <Link href={createDebugUrl(url)} className="underline">
+      Debug
+    </Link>{" "}
+    or see{" "}
+    <Link href="/examples" className="underline">
+      other examples
+    </Link>
+    <FrameContainer
+      postUrl="/frames"
+      pathname="/"
+      state={state}
+      previousFrame={previousFrame}
+    >
+      {/* <FrameImage src="https://framesjs.org/og.png" /> */}
+      <FrameImage aspectRatio="1.91:1">
+        <div tw="w-full h-full bg-blue-500 text-white justify-center items-center flex flex-col">
+          {/* <div tw="flex flex-row z-50">
+            {frameMessage?.inputText ? frameMessage.inputText : "Hello world"}
+          </div> */}
+          {frameMessage && (
+            <div tw="w-full h-full flex flex-col">
+              <img src = {imgs[state.pageIndex]!.src}/>
+            </div>
+          )}
+        </div>
+      </FrameImage>
+      <FrameInput text="put some text here" />
+      <FrameButton>
+        {state?.active === "1" ? "Active" : "Inactive"}
+      </FrameButton>
+      <FrameButton>
+        {state?.active === "2" ? "Active" : "Inactive"}
+      </FrameButton>
+      <FrameButton>A</FrameButton>
+      <FrameButton action="link" target={`https://www.google.com`}>
+        External
+      </FrameButton>
+    </FrameContainer>
+  </div>
+   
+    //     <FrameImage  
+    //       src={imgs[state.pageIndex]!.src}
+    //       aspectRatio="1:1"
+    //     >
+    //     <FrameInput
+    //       text="sign our newsletter"
 
-        />
-        <FrameButton>B</FrameButton>
-        <FrameButton>A</FrameButton>
-        <FrameButton>enter</FrameButton>
-        <FrameButton action="link" target={`https://www.youtube.com/`}>
-          Visit our site
-        </FrameButton>
-      </FrameContainer>
-    </div>
+    //     />
+    //     <FrameButton>B</FrameButton>
+    //     <FrameButton>A</FrameButton>
+    //     <FrameButton>{state?.active === "1" ? "Active" : "Inactive"}</FrameButton>
+    //     <FrameButton action="link" target={`https://www.youtube.com/`}>
+    //       Visit our site
+    //     </FrameButton>
+    //   </FrameContainer>
+    // </div>
   );
 }
